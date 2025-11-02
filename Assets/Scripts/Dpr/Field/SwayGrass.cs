476c57424c84949d90b8822be44449b55a18dd2a
VeryTwistedSpoon <39507107+VeryTwistedSpoon@users.noreply.github.com>
2025-11-02 13:48:26 +1000

    Poke Radar

--- Assets/Scripts/Dpr/Field/SwayGrass.cs
+++ Assets/Scripts/Dpr/Field/SwayGrass.cs
@@ -1,4 +1,5 @@
-﻿using Audio;
+﻿using AK;
+using Audio;
 using Dpr.Battle.Logic;
 using Pml;
 using UnityEngine;
@@ -27,52 +28,525 @@ namespace Dpr.Field
         public const int LOT_FAIL = 1;
         public const int LOT_OK = 2;
 
-        // TODO
-        public static void SwayGrass_InitSwayGrass() { }
+        public static void SwayGrass_InitSwayGrass() 
+        {
+            // Global Sway reset
+            is_swaygrass_flag = false;
+            swayZone = ZoneID.UNKNOWN;
+            rensa_count = 0;
+            rensa_mons = 0;
+            rensa_lv = 0;
+            work_data = null;
 
-        // TODO
-        public static void StopSE() { }
+            if (RootGrass == null)
+            {
+                RootGrass = new GameObject();
+                RootGrass.name = "SwayGrass Root";
+            }
 
-        // TODO
-        public static bool SwayGrass_CheckSpEncount(ref FieldEncount.SWAY_ENC_INFO info, ref Vector3 pos, float size) { return false; }
+            for (int i = 0; i < grass_data.Length; i++)
+            {
+                if (grass_data[i] == null)
+                {
+                    grass_data[i] = new GrassData();
+                    grass_data[i].transObject = new GameObject();
+                    grass_data[i].transObject.transform.SetParent(RootGrass.transform);
+                    grass_data[i].transObject.name = string.Format("grass {0}", i);
+                }
+
+                else
+                {
+                    grass_data[i].enable = false;
+                    grass_data[i].effectTime = 0f;
+                    grass_data[i].rensaMons = false;
+                    grass_data[i].rank = 0;
+                    grass_data[i].random_iro = 0;
+                    grass_data[i].random_kakure = 0;
+                    grass_data[i].attricode = -1;
+                }
+            }
+
+            StopSE();
+            TairyouHasseiPokeManager.ForceStop = false;
+            PlayerWork.PoketchData.ResetTempPoketore();
+        }
+
+        public static void StopSE()
+        {
+            for (int i = 0; i < _grassAudio.Length; i++)
+            {
+                if (_grassAudio[i] != null)
+                {
+                    _grassAudio[i].Stop();
+                }
+                _grassAudio[i] = null;
+            }
+        }
+
+        public static bool SwayGrass_CheckSpEncount(ref FieldEncount.SWAY_ENC_INFO info, ref Vector3 pos, float size) 
+        {
+            int swayGrassIndex = CheckSwayGrass(ref pos, size);
+            if (swayGrassIndex == -1)
+                return false;
+
+            work_data = grass_data[swayGrassIndex];
+            if (rensa_count == 0)
+            {
+                info.Decide = false;
+                info.Table = (RandomGroupWork.RandomValue(100) < 20) ? 0 : 1; // 20% chance for Table = 0, otherwise Table = 1
+                return true;
+            }
+
+            else
+            {
+                info.Decide = true;
+                info.Table = 0;
+                return true;
+            }
+        }
 
         public static bool SwayGrass_CheckValid()
         {
             return is_swaygrass_flag;
         }
 
-        // TODO
-        public static int CheckSwayGrass(ref Vector3 pos, float size) { return 0; }
+        public static int CheckSwayGrass(ref Vector3 pos, float size) 
+        {
+            if (!is_swaygrass_flag)
+                return -1;
 
-        // TODO
-        private static bool GetChainFlg(int inRound, BtlResult inBattleResult) { return false; }
+            for (int i = 0; i < grass_data.Length; i++)
+            {
+                GrassData tile = grass_data[i];
+                if (!tile.enable)
+                    continue;
 
-        // TODO
-        public static void SwayGrass_ChargePokeSearcher(byte diff) { }
+                float dy = System.Math.Abs(tile.position.y - pos.y);
+                if (dy > 1.0f)
+                    continue;
 
-        // TODO
-        public static int LotSwayGrass(ref Vector2Int grid, float height) { return 0; }
+                float dx = System.Math.Abs(tile.position.x - pos.x);
+                float dz = System.Math.Abs(tile.position.z - pos.z);
+                if (dx <= size && dz <= size)
+                    return i;
+            }
 
-        // TODO
-        private static void OnAudioInstanceFinished(AudioInstance instance) { }
+            return -1;
+        }
+        private static bool GetChainFlg(int inRound, BtlResult inBattleResult)
+        {
+            int rng = RandomGroupWork.RandomValue(100);
+            int continueChain;
 
-        // TODO
-        public static bool SwayZone() { return false; }
+            if (inBattleResult == BtlResult.BTL_RESULT_WIN)
+            {
+                switch (inRound)
+                {
+                    case 1:
+                        continueChain = 53;
+                        break;
+                    case 2:
+                        continueChain = 63;
+                        break;
+                    case 3:
+                        continueChain = 73;
+                        break;
+                    case 4:
+                        continueChain = 83;
+                        break;
+                    default:
+                        continueChain = 0;
+                        break;
+                }
+            }
+            else if (inBattleResult == BtlResult.BTL_RESULT_CAPTURE)
+            {
+                switch (inRound)
+                {
+                    case 1:
+                        continueChain = 63;
+                        break;
+                    case 2:
+                        continueChain = 73;
+                        break;
+                    case 3:
+                        continueChain = 83;
+                        break;
+                    case 4:
+                        continueChain = 93;
+                        break;
+                    default:
+                        continueChain = 0;
+                        break;
+                }
+            }
+            else
+            {
+                continueChain = 0;
+            }
 
-        // TODO
-        public static void Update(float deltatime) { }
+            return rng < continueChain;
+        }
 
-        // TODO
-        private static void PlayEffect(float deltatime, int i) { }
+        public static void SwayGrass_ChargePokeSearcher(byte diff) 
+        {
+            var itemInfo = ItemWork.GetItemInfo((int)ItemNo.POKETORE);
+            if (itemInfo == null || itemInfo.count <= 0)
+                return;
 
-        // TODO
-        public static int RensaNum(uint rensa, bool omamori = false) { return 0; }
+            var encData = PlayerWork.Enc_SV_Data;
 
-        // TODO
-        public static void BtlResultRensa(BtlResult result) { }
+            byte newCharge = (byte)(encData.PokeToreCharge + diff);
 
-        // TODO
-        public static byte RensaTalent() { return 0; }
+            encData.PokeToreCharge = (byte)((newCharge > POKETORE_CHARGE_MAX) ? POKETORE_CHARGE_MAX : newCharge);
+
+            PlayerWork.Enc_SV_Data = encData;
+        }
+
+        public static int LotSwayGrass(ref Vector2Int grid, float height) 
+        {
+            // Reset all slots 
+            for (int i = 0; i < grass_data.Length; i++)
+            {
+                if (grass_data[i] == null)
+                    grass_data[i] = new GrassData();
+
+                grass_data[i].enable = false;
+            }
+
+            // 9x9 grid around center tile
+            const int AREA = SWAY_GRASS_SEARCH_SIZE * SWAY_GRASS_SEARCH_SIZE;
+            int[] ranks = new int[AREA];
+            int[] attrCodes = new int[AREA];
+
+            int maxRankFound = 0;
+            int grassCount = 0;
+
+            for (int ry = 0; ry < SWAY_GRASS_SEARCH_SIZE; ry++)
+            {
+                for (int rx = 0; rx < SWAY_GRASS_SEARCH_SIZE; rx++)
+                {
+                    int index = ry * SWAY_GRASS_SEARCH_SIZE + rx;
+                    
+                    int dx = rx - 4;
+                    int dy = ry - 4;
+
+
+                    var cell = new Vector2Int(grid.x + dx, grid.y + dy);
+                    GameManager.GetAttribute(cell, out int attrId, out _);
+                    int code = GameManager.GetAttributeTable(attrId).Code;
+                    attrCodes[index] = code;
+
+                    if (!AttributeID.MATR_IsGrass(code))
+                    {
+                        ranks[index] = 0;
+                        continue;
+                    }
+
+                    // Ring is mapped by Chebyshev distance from the center (4,4)
+                    // This is explicitely written out, as there are no calls to math libraries here
+                    if (dx == -4 || dx == 4 || dy == -4 || dy == 4)
+                        ranks[index] = 4;
+                    else if (dx == -3 || dx == 3 || dy == -3 || dy == 3)
+                        ranks[index] = 3;
+                    else if (dx == -2 || dx == 2 || dy == -2 || dy == 2)
+                        ranks[index] = 2;
+                    else if (dx == -1 || dx == 1 || dy == -1 || dy == 1)
+                        ranks[index] = 1;
+
+                    if (ranks[index] > maxRankFound)
+                        maxRankFound = ranks[index];
+
+                    grassCount++;
+                }
+            }
+
+            if (grassCount == 0)
+                return LOT_NO_GRASS;
+
+            bool allDisabled = false;
+
+            int iroRange = RensaNum(rensa_count, false);
+            int swayPatchNo = grassCount >= SWAY_GRASS_MAX ? SWAY_GRASS_MAX : grassCount;
+            if (swayPatchNo < 1)
+                allDisabled = true;
+
+            int threshold = maxRankFound;
+
+            for (int i = 0; i < swayPatchNo; i++)
+            {
+                int tileIndex;
+                do
+                {
+                    tileIndex = RandomGroupWork.RandomValue(AREA);
+                }
+                while (ranks[tileIndex] < threshold);
+
+                // 9x9 index back to grid
+                int rx = tileIndex % SWAY_GRASS_SEARCH_SIZE;
+                int ry = (tileIndex / SWAY_GRASS_SEARCH_SIZE) % AREA;
+                int dx = rx - 4;
+                int dy = ry - 4;
+                var cell = new Vector2Int(grid.x + dx, grid.y + dy);
+
+                // Position from grid - Initial Y = height + 2, then raycast down to ground.
+                Vector2 pos2 = FieldObjectEntity.GridToPosition(cell);
+                grass_data[i].position = new Vector3(pos2.x, height + 2.0f, pos2.y);
+
+                bool raycastHit = Physics.Raycast(grass_data[i].position, new Vector3(0.0f, -1.0f, 0.0f), out var hitInfo, 3.0f, Layer.Ground);
+                if (raycastHit)
+                    grass_data[i].position.y = hitInfo.point.y;
+                grass_data[i].enable = raycastHit;
+
+                grass_data[i].effectTime = 0.0f;
+                grass_data[i].random_iro = RandomGroupWork.RandomValue(iroRange);
+                grass_data[i].random_kakure = RandomGroupWork.RandomValue(128);
+                grass_data[i].rank = ranks[tileIndex];
+                grass_data[i].attricode = attrCodes[tileIndex];
+
+                ranks[tileIndex] = -1;
+                threshold -= 2;
+                if (threshold < 2)
+                    threshold = 1;
+            }
+
+            for (int i = 0; i < grass_data.Length; i++)
+                allDisabled &= !grass_data[i].enable;
+
+            if (allDisabled)
+                return LOT_FAIL;
+
+            swayZone = PlayerWork.zoneID;
+            is_swaygrass_flag = true;
+            TairyouHasseiPokeManager.ForceStop = true;
+
+            // Stop existing audio and spawn patches + audio per enabled patch
+            for (int i = 0; i < grass_data.Length; i++)
+            {
+                _grassAudio[i]?.Stop();
+
+                if (!grass_data[i].enable)
+                    continue;
+
+                if (grass_data[i].transObject == null)
+                {
+                    grass_data[i].transObject = new GameObject();
+                    grass_data[i].transObject.transform.SetParent(RootGrass.transform);
+                    grass_data[i].transObject.name = string.Format("grass {0}", i);
+                }
+                grass_data[i].transObject.transform.position = grass_data[i].position;
+
+                _grassAudio[i] = AudioManager.Instance.CreateSe(EVENTS.S_ENV001, EVENTS.STOP_S_ENV001, Vector3.zero, Quaternion.identity, grass_data[i].transObject.transform);
+                _grassAudio[i].Play(OnAudioInstanceFinished);
+            }
+
+            _callSwayBGM = true;
+            return LOT_OK;
+        }
+
+        private static void OnAudioInstanceFinished(AudioInstance instance) 
+        {
+            for (int i = 0; i < grass_data.Length; i++)
+            {
+                if (_grassAudio[i] != null && _grassAudio[i] == instance)
+                {
+                    _grassAudio[i] = null;
+                    return;
+                }
+            }
+        }
+
+        public static bool SwayZone() 
+        {
+            return PlayerWork.zoneID == swayZone;
+        }
+
+        public static void Update(float deltatime) 
+        {
+            if (!is_swaygrass_flag)
+                return;
+            
+            if (!SwayZone())
+                {
+                    SwayGrass_InitSwayGrass();
+                    return;
+                }
+
+            for (int i = 0; i < grass_data.Length; i++)
+            {
+                if (grass_data[i].enable)
+                {
+                    PlayEffect(deltatime, i);
+                }
+            }
+        }
+
+        private static void PlayEffect(float deltatime, int i) 
+        {
+            ref var grassData = ref grass_data[i];
+
+            if (grassData.effectTime > 0f)
+            {
+                grassData.effectTime -= deltatime;
+                return;
+            }
+
+            EffectFieldID effectIndex;
+            switch ((EffectFieldID)grassData.attricode)
+            {
+                case EffectFieldID.EF_F_ENCOUNT_TRAINER_ICE_01:
+                    effectIndex = EffectFieldID.EF_F_GRASS_03_SHAKE_01;
+                    break;
+                case EffectFieldID.EF_F_WEATHER_RAIN:
+                    effectIndex = EffectFieldID.EF_F_GRASS_02_SHAKE_01;
+                    break;
+                default:
+                    effectIndex = EffectFieldID.EF_F_GRASS_04_SHAKE_01;
+                    break;
+            }
+
+            if (grassData.random_kakure == 0)
+                effectIndex = (EffectFieldID)((int)effectIndex + 1);
+
+            FieldManager.Instance.CallEffect(effectIndex, grassData.position, null, null);
+
+            if (grassData.random_iro == 0)
+                FieldManager.Instance.CallEffect(EffectFieldID.EF_F_GRASS_SPARKLE, grassData.position, null, null);
+
+            grassData.effectTime = (RandomGroupWork.Value() * 15f + 1f) / 30f;
+        }
+
+        public static int RensaNum(uint rensa, bool omamori = false) 
+        {
+            int[] shinyOdds = new int[41]             
+            {
+                4096, 3855, 3640, 3449, 3277, 3105, 2723, 2849, 2731, 2621,
+                2521, 2405, 2347, 2213, 2113, 2049, 1986, 1927, 1856, 1791,
+                1451, 1351, 1232, 1067, 939, 822, 705, 500, 389, 364,
+                335, 296, 225, 159, 120, 104, 69, 481, 799, 200, 99
+            };
+
+            int[] shinyCharmOdds = new int[41]         
+            {
+                3640, 3449, 3277, 3121, 2979, 2849, 2731, 2621, 2521, 2427,
+                2341, 2259, 2185, 2114, 2048, 1986, 1927, 1872, 1820, 1771,
+                1724, 1680, 1638, 1598, 1560, 1524, 1489, 1456, 1424, 1394,
+                1260, 1236, 1213, 1192, 1170, 1149, 963, 780, 390, 195, 97
+            };
+
+
+            int[] oddsTable = omamori ? shinyCharmOdds : shinyOdds;
+
+            if (rensa < oddsTable.Length)
+                return oddsTable[rensa];
+            else
+                return oddsTable[oddsTable.Length - 1];
+        }
+
+        public static void BtlResultRensa(BtlResult result) 
+        {
+            if (!is_swaygrass_flag)
+                return;
+
+            if (work_data == null)
+            {
+                SwayGrass_InitSwayGrass();
+                return;
+            }
+
+            rensa_count += 1;
+
+            if (rensa_count >= MAX_RENSA_COUNT)
+                rensa_count = MAX_RENSA_COUNT;
+
+            PlayerWork.PoketchData.PoketoreSetCount((ushort)rensa_mons, (int)rensa_count);
+
+            int continueChain = 0;
+
+            if (result == BtlResult.BTL_RESULT_CAPTURE)
+            {
+                switch (work_data.rank)
+                {
+                    case 1: 
+                        continueChain = 63; 
+                        break;
+                    case 2: 
+                        continueChain = 73; 
+                        break;
+                    case 3: 
+                        continueChain = 83; 
+                        break;
+                    case 4: 
+                        continueChain = 93; 
+                        break;
+                }
+            }
+
+            else if (result == BtlResult.BTL_RESULT_WIN)
+            {
+                switch (work_data.rank)
+                {
+                    case 1:
+                        continueChain = 53;
+                        break;
+                    case 2:
+                        continueChain = 63;
+                        break;
+                    case 3:
+                        continueChain = 73;
+                        break;
+                    case 4:
+                        continueChain = 83;
+                        break;
+                }
+            }
+
+            if (RandomGroupWork.RandomValue(100) < continueChain)
+            {
+                work_data = null;
+            }
+
+            else
+            {
+                SwayGrass_InitSwayGrass();
+                _callStopSwayBGM = true;
+            }
+
+            if (is_swaygrass_flag)
+            {
+                _callSwayBGM = true;
+                BattleEndRensaStart = true;
+            }
+        }
+
+        public static byte RensaTalent() 
+        {
+            if (!is_swaygrass_flag)
+                return 0;
+
+            var rensa = rensa_count + 1;
+            if (rensa >= MAX_RENSA_COUNT)
+                rensa = MAX_RENSA_COUNT;
+
+            switch (rensa)
+            {
+                case 0:
+                    return 0;
+                case 20:
+                    return 1;
+                case 30:
+                    return 2;
+                case 40:
+                    return 3;
+                case 100:
+                    return 5;
+                default:
+                    if (rensa % 100 == 0)
+                        return 5;
+                    else
+                        return 0;
+            }
+        }
 
         public class GrassData
         {
